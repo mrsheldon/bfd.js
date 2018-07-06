f542571654edddc453e5f045ae9d52cdaee915fe
Roy <MrSheldon@Luki.xyz>
2018-07-07 02:13:42 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -5,21 +5,10 @@ Simply Run `npm install bfd.js`
 
 # Example
 ```javascript
-const BFD = require('bfd.js');
+onst BFD = require('bfd.js');
 const BFDClient = new BFD('Bot ID', 'Bot Token');
 
 BFDClient.update(client.guilds.size)
 
-    console.log("Server Count Updated")
-
-BFDClient.getInfo('365958655926992896').then(bot => {
-
-    console.log("Current Bot Username: "+ bot.prefix),
-});
-
- BFDClient.getCurrentBot().then(bot => {
-
-    console.log("Current Bot Username: "+ bot.prefix),
-
-});
+console.log("Server Count Updated")
 ```
--- src/index.js
+++ src/index.js
@@ -14,22 +14,6 @@ class Client {
         snekfetch.post(this._baseURL + '/bots/' + this._id, {headers: { 'content-type': 'application/json', 'authorization': this._token }, data: {server_count: serverCount } })
 
         }
-    getInfo(id) {
-         if (typeof id !== 'string') throw new TypeError('Bot ID must be a string');
-        return new Promise((resolve, reject) => {
-            snekfetch.get(this._baseURL + '/bots/' + id).then((bot) => {
-                resolve(bot.body);    
-            })
-        });
-    }
-    getCurrentBot() {
-        if (typeof this._id !== 'string') throw new TypeError('Bot ID must be a string');
-        return new Promise((resolve, reject) => {
-            snekfetch.get(this._baseURL + '/bots/' + this._id).then((bot) => {
-                resolve(bot.body);
-            })
-        });
-    }
 }
 
 
