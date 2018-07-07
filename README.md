a7cc9fd38adefab739f1128a789904fddc9781a4
Roy <MrSheldon@Luki.xyz>
2018-07-07 17:51:15 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,14 +1,37 @@
-[![NPM](https://nodei.co/npm/bfd.js.png?downloads=true&downloadRank=true&stars=true)](https://npmjs.com/package/bfd.js/)
+[https://nodei.co/npm/jstrip.png?downloads=true&downloadRank=true&stars=true](https://npmjs.com/package/bfd.js)
 
 # Getting Started
 Simply Run `npm install bfd.js`
 
 # Example
+Post server count
+```javascript
+const BFD = require('bfd.js');
+
+const BFDClient = new BFD('Bot ID', 'Bot Token');
+
+BFDClient.postStats(client.guilds.size)
+console.log("Server Count Updated")
+```
+Get bot info
 ```javascript
 const BFD = require('bfd.js');
 const BFDClient = new BFD('Bot ID', 'Bot Token');
 
-BFDClient.update(client.guilds.size)
+DBGClient.getInfo('365958655926992896').then(bot => {
 
-console.log("Server Count Updated")
+    console.log("Current Bot Username: "+ bot.username),
+    console.log("Current Bot ID: "+ bot.id),
+})
+```
+Get currect bot info
+```javascript
+const BFD = require('bfd.js');
+const BFDClient = new BFD('Bot ID', 'Bot Token');
+ DBGClient.getCurrentBot().then(bot => {
+
+    console.log("Current Bot Username: "+ bot.username),
+    console.log("Current Bot ID: "+ bot.discordid),
+
+}) 
 ```
--- src/index.js
+++ src/index.js
@@ -9,12 +9,29 @@ class Client {
         this._id = id;
     }
 
-    update(serverCount) {
+    postStats(serverCount) {
         if (typeof serverCount !== 'number' && !(serverCount instanceof Array)) throw new TypeError('Server count is not a number.');
-        snekfetch.post(this._baseURL + '/bots/' + this._id, {headers: { 'content-type': 'application/json', 'authorization': this._token }, data: {server_count: serverCount } })
+        snekfetch.post(this._baseURL + '/bots/' + this._id, {headers: { 'content-type': 'application/json', 'authorization': this._token }, data: {count: serverCount } })
 
         }
-}
 
+    getInfo(id) {
+        if (typeof id !== 'string') throw new TypeError('Bot ID must be a string');
+        return new Promise((resolve, reject) => {
+            snekfetch.get(this._baseURL + '/bots/' + id).then((bot) => {
+                resolve(bot.body);    
+            })
+        });
+    }
+
+    getCurrentBot() {
+        if (typeof this._id !== 'string') throw new TypeError('Bot ID must be a string');
+        return new Promise((resolve, reject) => {
+            snekfetch.get(this._baseURL + '/bots/' + this._id).then((bot) => {
+                resolve(bot.body);
+            })
+        });
+    }
+}
 
 module.exports = Client;
